b27389a3502fd7458da9d04389912a44f601588a
AugusCh <128873139+AugusCh@users.noreply.github.com>
2025-10-22 03:48:32 +0000

    Deploying to gh-pages from @ AugusCh/AugusCh.github.io@a854ea675b71e1f7e657bbf36c946916e785dd0b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AugusCh/INS_Website",
+    "data-repo": "AugusCh/AugusCh.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
